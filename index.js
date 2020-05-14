dcfb7b4e51a4fb85eee645b7f3f95e479cb3fc8d
Ivan Llopis <ivllobel@gmail.com>
2020-05-14 03:07:27 +0200

    IMDB API working for the carousel

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const axios = require('axios');
-const qs = require('qs');
+//const qs = require('qs');
 require('dotenv').config();
 
 const seriesRouter = require('./routes/series');
@@ -11,17 +11,21 @@ const seriesRouter = require('./routes/series');
 //In-memory storage of logged-in users
 // For demo purposes only, production apps should store this in a reliable storage
 
-var users = {};
+//var users = {};
 
 const app = express();
 
 app.use(express.static('public')); //serves automatically index.html
+app.use('/series', seriesRouter);
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'));
+  res.redirect('/series');
+  //res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
-app.use('/series', seriesRouter);
+app.get('/movies', (req, res) => {
+  res.redirect('/series');
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
--- routes/series.js
+++ routes/series.js
@@ -1,56 +1,52 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
+const axios = require('axios');
 
-const series = [
-    {
-      slide_num: 0,
-      source: "pictures/Astral_Waste.png",
-      description: "This is a description for the Astral Waste item in Dark Souls III."
-    },
-    {
-      slide_num: 1,
-      source: "pictures/Astrologist_Icon.png",
-      description: "This is a description for the Astrologist item in Dark Souls III."
-    },
-    {
-      slide_num: 2,
-      source: "pictures/Star_Jelly.png",
-      description: "This is a description for the Star Jelly item in Dark Souls III."
-    },
-    {
-      slide_num: 3,
-      source: "pictures/Star_Nectar.png",
-      description: "This is a description for the Star Nectar item in Dark Souls III."
-    },
-    {
-      slide_num: 4,
-      source: "pictures/Astral_Waste.png",
-      description: "This is a description for the Astral Waste item in Dark Souls III."
-    },
-    {
-      slide_num: 5,
-      source: "pictures/Astrologist_Icon.png",
-      description: "This is a description for the Astrologist item in Dark Souls III."
-    },
-    {
-      slide_num: 6,
-      source: "pictures/Star_Jelly.png",
-      description: "This is a description for the Star Jelly item in Dark Souls III."
-    },
-    {
-      slide_num: 7,
-      source: "pictures/Star_Nectar.png",
-      description: "This is a description for the Star Nectar item in Dark Souls III."
+const series_names = ['Brooklyn Nine-Nine', 'Game of Thrones', 'Lost', 'Big Mouth', 'Rick and Morty', 'Naruto Shippuden', 'Dragon Ball Z', 'Vikings', 'After Life', 'Lucifer', 'The Witcher'];
+var simpleDatabase = [];
+
+async function getSeries() {
+    for(let serie of series_names) {
+        try {
+            const dataSerie = await getSerie(serie);
+            simpleDatabase.push(dataSerie.data);
+            //console.log(serie);
+            //console.log(dataSerie.data);
+        } catch (error) {
+            console.error(error);
+        }
     }
-]
+    //return simpleDatabase;
+}
+
+async function getSerie(nameSerie) {
+    try {
+            return axios.get('http://www.omdbapi.com/', {
+              params: {
+                  apikey: process.env.OMDb_API_KEY,
+                  t: nameSerie
+              }
+            });
+    } catch (error) {
+            console.error(error);
+    }
+}
+
+// Make sure the database is up to date (In the future this will be triggered by the user!)
+getSeries();
 
 router.get('/', (req, res) => {
-    res.send("You are interested in series.")
+    res.sendFile(path.join(__dirname, '../public/series.html'));
 });
 
 router.get('/any', (req, res) => {
-    const recommended_random_serie = Math.round(Math.random() * series.length);
-    res.json(series[recommended_random_serie]);
+    const recommended_random_serie = Math.round(Math.random() * simpleDatabase.length);
+    res.json({indexSerie: recommended_random_serie, data: simpleDatabase[recommended_random_serie]});
+});
+
+router.get('/infoSeries', async (req, res) => {
+    res.json(simpleDatabase);
 });
 
 router.get('/:id', (req, res) => {
@@ -58,8 +54,8 @@ router.get('/:id', (req, res) => {
         const indexSerie = parseInt(req.params.id);
         if(isNaN(indexSerie)){
             res.send("This serie is not valid."); //normally we would flash around this error!
-        } else if(indexSerie > series.length) res.send("This series does not exist.");
-        else res.json(series[parseInt(req.params.id)]);
+        } else if(indexSerie > simpleDatabase.length) res.send("This series does not exist.");
+        else res.json({indexSerie: indexSerie, data: simpleDatabase[indexSerie]});
     }catch(e){
         console.log(e);
     }

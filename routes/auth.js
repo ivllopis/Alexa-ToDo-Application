01972a7a0b290adfc6dc81e86a666c635177c1fa
Ivan Llopis <ivllobel@gmail.com>
2026-02-15 12:54:34 +0100

    Bump packages to use express 5 and axios 1.13.5

--- routes/auth.js
+++ routes/auth.js
@@ -46,8 +46,10 @@ router.post('/login', async (req, res) => {
 });
 
 router.get('/logout', (req, res) => {
-    req.session.destroy();
-    res.redirect('/');
+    req.session.destroy((err) => {
+        if (err) console.error('Session destroy error:', err);
+        res.redirect('/');
+    });
 });
 
 module.exports = router;
